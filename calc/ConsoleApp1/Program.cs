afa34b8a98f3276eb99f349ad913b548a9ddc2e8
dropstar1992@gmail.com <dropstar1992@gmail.com>
2018-12-15 22:45:08 +0300

    1

---
+++ calc/ConsoleApp1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char again = 'д';
+
+            while (again == 'д')
+
+            {
+
+                double a;
+
+                double b;
+
+                double total;
+
+                char oper;
+
+
+                    Console.WriteLine("Введите первое число:");
+
+                    a = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Введите оператор:");
+
+                    oper = Convert.ToChar(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число:");
+
+                    b = Convert.ToDouble(Console.ReadLine());
+
+                if (oper == '+')
+
+                {
+
+                    total = a + b;
+
+                    Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+
+                }
+
+                else if (oper == '-')
+
+                {
+
+                    total = a - b;
+
+                    Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+
+                }
+
+                else if (oper == '*')
+
+                {
+
+                    total = a * b;
+
+                    Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+
+                }
+
+                else if (oper == '/')
+
+                {
+
+                    total = a / b;
+
+                    Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+
+                }
+
+                else
+
+                {
+
+                    Console.WriteLine("Неизвестный оператор, введите +-/= .");
+
+                }
+
+                Console.WriteLine("Вам нужно продолжить работу с калькулятором? (д/н)");
+
+                again = Convert.ToChar(Console.ReadLine());
+
+            }
+
+        }
+    }
+}
