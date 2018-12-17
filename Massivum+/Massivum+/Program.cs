f71e985c48a7352cea5b295e8107504cf3054ca8
Nikolai Markin <45819988+MarkinNikolai@users.noreply.github.com>
2018-12-17 16:58:31 +0300

    Массивы
    1

---
+++ Massivum+/Massivum+/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Massivum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк массивов");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов массивов");
+            int a = int.Parse(Console.ReadLine());
+
+            int[,] m1 = new int[a, b];
+            Console.WriteLine("Заполните данные для первого массива");
+            for (int k = 0; k < a; k++)
+            {
+                for (int l = 0; l < b; l++)
+                {
+                    Console.Write("mas[" + k + "," + l + "]: ");
+
+                    m1[k, l] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int[,] m2 = new int[a, b];
+            Console.WriteLine("Заполните данные для второго массива");
+            for (int k = 0; k < a; k++)
+            {
+                for (int l = 0; l < b; l++)
+                {
+                    Console.Write("mas[" + k + "," + l + "]: ");
+                    m2[k, l] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int[,] msum = new int[a, b];
+            for (int k = 0; k < a; k++)
+            {
+                for (int l = 0; l < b; l++)
+                {
+                    msum[k, l] = m1[k, l] + m2[k, l];
+                }
+            }
+
+            Console.WriteLine("Итоговый массив");
+            for (int k = 0; k < a; k++)
+            {
+                for (int l = 0; l < b; l++)
+                {
+                    Console.Write(msum[k, l] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+}
